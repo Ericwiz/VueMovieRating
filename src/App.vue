80f026a1cc9c33be256f8e65030856c3de66ca6a
Ericwiz <wisdomagu44@gmail.com>
2023-06-14 21:14:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold text-center underline">Hello World Vue.js</h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>

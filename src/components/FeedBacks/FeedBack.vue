6c9ddc7516bbbf5f65bba1bec82bc47e76710490
Ericwiz <wisdomagu44@gmail.com>
2023-06-22 20:25:23 +0100

    review and rating

---
+++ src/components/FeedBacks/FeedBack.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MovieReview from './MovieReview.vue';
+
+</script>
+
+<template>
+    <div class="bg-[#191517] border-t-2 border-t-pink-800">
+        <div class="bg-[#0f1010] px-4 sm:px-8 md:px-16 shadow-lg shadow-black">
+            <h1 class="uppercase text-white text-lg xs:text-2xl md:text-3xl lg:text-4xl py-8 2xl:text-5xl">Reviews and recommendations</h1>
+            <div class="flex justify-start space-x-6 xs:space-x-10 pt-8">
+            </div>
+        </div>
+        <MovieReview />
+    </div>
+</template>
---
+++ src/components/FeedBacks/MovieReview.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ReviewCard from './ReviewCard.vue'
+import MovieRecommendation from './MoviewRecommendation.vue'
+</script>
+
+<template>
+    <div class="flex flex-col-reverse lg:flex-row justify-between space-x-10 pt-8 pb-3 pb-5 px-4 sm:px-8 md:px-16">
+        <ReviewCard />
+        <MovieRecommendation />
+    </div>
+</template>

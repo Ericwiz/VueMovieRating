85217da0f454979cf28d849d1adcdeb14d840366
Ericwiz <wisdomagu44@gmail.com>
2023-06-22 20:50:40 +0100

    review and rating

---
+++ src/composables/useFeedback.js
@@ -0,0 +1,33 @@
+import { ref } from "vue"
+
+
+export default function useFeedback() {
+    const author = ref('')
+    const id = ref(0)
+    const rating = ref(5)
+    const feedbackText = ref('')
+    const movieId = ref('')
+
+    let feedBacks = ref([])
+
+    function addReview() {
+        feedBacks.value.push({id: id.value+=1, author: author.value, rating: rating.value, feedbackText: feedbackText.value, movieId: movieId})
+
+          author.value = ''
+          rating.value = 1
+          feedbackText.value = ''
+         
+         return localStorage.setItem('reviews', JSON.stringify(
+            feedBacks.value
+        ))
+    }
+
+    function addComment() {
+        return feedBacks.value.push({id: id.value+=1, author: author, rating: rating, feedbackText: feedbackText})
+    }
+
+
+
+    return { addReview, addComment, author, id, feedBacks, rating, feedbackText, movieId}
+    
+}

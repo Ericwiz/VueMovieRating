05a65724759323cb54a4086d162470304e00a423
Ericwiz <wisdomagu44@gmail.com>
2023-06-23 19:23:55 +0100

    style change

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -2,7 +2,7 @@
   <article class="pt-32 bg-[#2b2b31] h-full">
     <section
       v-if="movie"
-      class="flex flex-col md:flex-row justify-between space-x-6 pb-24 px-4 sm:px-8 md:px-16"
+      class="flex flex-col md:flex-row md:justify-between space-x-6 pb-24 px-4 sm:px-8 md:px-16"
     >
       <div class="w-full md:w-[60%]">
         <img
@@ -11,30 +11,30 @@
           class="rounded-sm w-full h-full md:w-96 md:h-96 xl:w-full xl:h-[70%]"
         />
       </div>
-      <div class="flex flex-col space-y-2 2xl:space-y-4 w-full">
-        <h1 class="text-lg sm:text-2xl pt-2 md:pt-0 md:text-3xl 2xl:text-4xl text-white">
+      <div class="flex flex-col w-full space-y-2 2xl:space-y-4">
+        <h1 class="text-lg sm:text-2xl -ml-6 md:-ml-0 pt-2 md:pt-0 md:text-3xl 2xl:text-4xl text-white">
           {{ movie.Title }}
         </h1>
-        <p class="font-normal 2xl:text-2xl pt-5">
+        <p class="font-normal -ml-6 md:-ml-0 2xl:text-2xl pt-5">
           <span class="text-lg xl:text-2xl text-white pr-4">Genre:</span>
           <span class="text-pink-600 text-small">{{ movie.Genre }}</span>
         </p>
 
-        <p class="font-normal xl:text-2xl">
+        <p class="font-normal -ml-6 md:-ml-0 xl:text-2xl">
           <span class="text-lg xl:text-2xl text-white pr-4">Released:</span>
           <span class="text-white text-small">{{ movie.Released }}</span>
         </p>
 
-        <p class="font-normal 2xl:text-2xl">
+        <p class="font-normal -ml-6 md:-ml-0 2xl:text-2xl">
           <span class="text-lg xl:text-2xl text-white pr-4">Runtime:</span>
           <span class="text-white text-small">{{ movie.Runtime }}</span>
         </p>
 
-        <p class="font-normal 2xl:text-2xl">
+        <p class="font-normal -ml-6 md:-ml-0 2xl:text-2xl">
           <span class="text-lg xl:text-2xl text-white pr-4">Country:</span>
           <span class="text-pink-600 uppercase text-small">{{ movie.Country }}</span>
         </p>
-        <div class="flex space-x-5 pb-5">
+        <div class="flex -ml-6 md:-ml-0 space-x-5 pb-5">
           <div class="flex space-x-2">
             <Icon icon="material-symbols:star" class="flex text-pink-600 h-6 2xl:h-8 w-6 2xl:w-8" />
             <span class="text-white font-bold 2xl:text-2xl">{{ movie.imdbRating }}</span>
